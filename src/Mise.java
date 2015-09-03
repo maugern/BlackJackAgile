0ae6a2b73b8ebc83c4d526867268014164487aab
Denoeud Antoine <denoeudantoine@laposte.net>
2015-09-03 17:11:07 +0200

    Ajout de la mise et trucs

--- src/Mise.java
+++ src/Mise.java
@@ -1,7 +1,9 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,10 +11,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.border.Border;
 
 public class Mise {
 
 	private int mise = 1;
+	private JLabel cagnotte = new JLabel();
 	private JButton augmenter = new JButton("Augmenter");
 	private JButton diminuer = new JButton("Diminuer");
 	private JButton texte = new JButton("Mise : " + mise);
@@ -22,9 +26,12 @@ public class Mise {
 				.getImage());
 		f.setPreferredSize(new Dimension(200, 300));
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setLayout(new GridLayout(3, 1));
+		f.setLayout(new GridLayout(4, 1));
 
 		
+		cagnotte.setText("Votre cagnotte : " + j1.getJetons());
+		cagnotte.setFont(new Font("Helvetica", 0, 16));
+		cagnotte.setHorizontalAlignment(JLabel.CENTER);
 		augmenter.setBackground(Color.GREEN);
 		augmenter.setForeground(Color.WHITE);
 		augmenter.setFocusable(false);
@@ -34,49 +41,53 @@ public class Mise {
 		texte.setBackground(Color.WHITE);
 		texte.setFocusable(false);
 		texte.setFont(new Font("Helvetica", 1, 24));
-		
+
 		augmenter.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(mise < j1.getJetons()){
+				if (mise < j1.getJetons()) {
 					mise++;
 					texte.setText("Mise : " + mise);
 				}
-				
+
 			}
 		});
-		
+
 		diminuer.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(mise > 1){
+				if (mise > 1) {
 					mise--;
 					texte.setText("Mise : " + mise);
 				}
-				
+
 			}
 		});
-		
+
 		texte.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				j1.setMise(mise);
-				j1.setJetons(j1.getJetons()-mise);
+				if (j1.getJetons() > 0) {
+					j1.setMise(mise);
+					j1.setJetons(j1.getJetons() - mise);
 
-				f.dispose();
-				final JFrame f = new JFrame("BlackJack");
-				
-				javax.swing.SwingUtilities.invokeLater(new Runnable() {
-					public void run() {
-						new Jeu(f,j1);
-					}
-				});
+					f.dispose();
+					final JFrame f = new JFrame("BlackJack");
+
+					javax.swing.SwingUtilities.invokeLater(new Runnable() {
+						public void run() {
+							new Jeu(f, j1);
+						}
+					});
+				}
+
 			}
 		});
-		
+
+		f.getContentPane().add(cagnotte);
 		f.getContentPane().add(augmenter);
 		f.getContentPane().add(texte);
 		f.getContentPane().add(diminuer);
