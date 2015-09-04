9e70952c3f5ba72a8a85664a7e3589c1d15b0531
Jnuxxx <jinux.f59@gmail.com>
2015-09-04 09:26:52 +0200

    Modification TestCroupier

--- test/TestCroupier.java
+++ test/TestCroupier.java
@@ -1,15 +1,39 @@
-import junit.frame.Asert;
-import org.junit.Test;
-import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
 
 public class TestCroupier{
-  private Croupier reference;
+  private Croupier reference = new Croupier();
   private ArrayList<Carte> test_listeDeCarte = new ArrayList<Carte>();
-  test_listeDeCarte.add(new Carte(5,1));
-  test_listeDeCarte.add(new Carte(5,1));
+  
   
   @Test
   public void test_getValeurDeLaMain(){
-    reference.setListeDeCarte(test_listeDeCarte);
-    assertEquals(referance.getValeurDeLaMain(), 10);
+	test_listeDeCarte.add(new Carte(12,1));
+    reference.setListeCarteDuCroupier(test_listeDeCarte);
+    assertEquals(reference.getValeurDeLaMain(), 12);
+    
+    test_listeDeCarte.add(new Carte(1,2));
+    reference = new Croupier();
+    reference.setListeCarteDuCroupier(test_listeDeCarte);
+    assertEquals(reference.getValeurDeLaMain(), 13);
+    
+    test_listeDeCarte.add(new Carte(7,2));
+    reference = new Croupier();
+    reference.setListeCarteDuCroupier(test_listeDeCarte);
+    assertEquals(reference.getValeurDeLaMain(), 20);
   }
+  
+  @Test
+  public void test_pioche(){
+	  reference = new Croupier();
+	  test_listeDeCarte.add(new Carte(2,3));
+	  reference.setListeCarteDuCroupier(test_listeDeCarte);
+	  reference.pioche(new Carte(5,1));
+	  assertEquals(reference.getListeCarteDuCroupier().size(), 4);
+	  
+	  reference.pioche(new Carte(5,3));
+	  assertEquals(reference.getListeCarteDuCroupier().size(), 4);
+  }
+}
